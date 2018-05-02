976c658e08137798ab216ac89168fd7ac539dc1f
Ovidiu <ovidiu.bunea@yahoo.com>
2018-05-02 19:09:03 +0300

    a doua versiune

--- src/Testing.java
+++ src/Testing.java
@@ -1,6 +1,11 @@
 public class Testing {
     public static void main(String[] args) {
 
-        System.out.println("Boom boom");
+        int x=6;
+        int y=7;
+
+        int result =  x +  y;
+
+        System.out.println("Boom boom " +result );
     }
 }
